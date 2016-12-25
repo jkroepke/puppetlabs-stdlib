386933665985411c3a4c57054f16bad2ed14c587
Jan-Otto Kröpke <jan-otto.kroepke@noris.de>
2016-12-25 12:07:17 +0100

    Added tests

--- spec/aliases/ipv6_spec.rb
+++ spec/aliases/ipv6_spec.rb
@@ -5,7 +5,9 @@ if Puppet.version.to_f >= 4.5
     describe 'accepts ipv6 addresses' do
       [
         '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-        'fa76:8765:34ac:0823:ab76:eee9:0987:1111'
+        '2001:0db8:85a3:0000::4',
+        'fa76:8765:34ac:0823:ab76:eee9:0987:1111',
+        'fa76:8765:34ac:0823::1',
       ].each do |value|
         describe value.inspect do
           let(:params) {{ value: value }}
